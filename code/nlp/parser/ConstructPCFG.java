7876c0f48f89dd44bb4c0c87ffca54ea2e20bc86
Ezra Ford <ezraford44@gmail.com>
2023-02-20 09:59:24 -0800

    Complete

--- code/nlp/parser/ConstructPCFG.java
+++ code/nlp/parser/ConstructPCFG.java
@@ -1,10 +1,8 @@
 package nlp.parser;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Scanner;
 
 
 public class ConstructPCFG {
@@ -12,24 +10,33 @@ public class ConstructPCFG {
     protected HashMap<String, HashMap<GrammarRule, Double>> rules; //a hash map with the strings as keys and rules
     // and their counts as values
 
+
+    /**
+     * Given a file name, the method constructs a PCFG by reading through a file and running several functions on it.
+     *
+     * @param filename the file name of the file we are working with
+     *
+     */
     public ConstructPCFG(String filename) {
         // initialize counts & rules
-        counts = new HashMap<>();
-        rules = new HashMap<>();
+        counts = new HashMap<String, Double>();
+        rules = new HashMap<String, HashMap<GrammarRule, Double>>();
 
-        // process file
-        File file = new File(filename);
-
-        try (Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNextLine()) {
-                ParseTree pt = new ParseTree(scanner.nextLine());
+        // create bufferedReader, go through every line, and update the counts
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                ParseTree pt = new ParseTree(line);
                 updateCounts(pt);
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found: " + file);
-        }
 
-        updateWeights();
+            //update the weights and close the reader
+            updateWeights();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -72,99 +79,111 @@ public class ConstructPCFG {
      * Updates the weights of each GrammarRule in the tree
      */
     public void updateWeights() {
+
+        //goes over every String in the rules
         for (String key : rules.keySet()) {
+
+            //creates the hashmaps
             HashMap<GrammarRule, Double> temp = rules.get(key);
             HashMap<GrammarRule, Double> updatedRules = new HashMap<>();
+
+            //updates the weight by taking the count of the specific string and dividing it by the total
+            //number of counts of strings following that string
             for (GrammarRule rule : temp.keySet()){
-                //updates the weight by taking the count of the specific string and dividing it by the total
-                //number of counts of strings following that string
                 double numerator = temp.get(rule);
                 double denominator = counts.get(key);
                 double weight = numerator / denominator;
                 GrammarRule updatedRule = new GrammarRule(rule.getLhs(), rule.getRhs(), rule.isLexical());
                 updatedRule.setWeight(weight);
                 updatedRules.put(updatedRule, numerator);
-                System.out.println(rules.get(key).get(rule));
             }
             rules.put(key, updatedRules);
         }
     }
 
+    /**
+     * Binarizes the PCFG (starts with the leftmost pair of symbols and replace them with a new nonterminal,
+     * not already in the grammar, creates a new PCFG rule from X to those two symbols
+     * with probability 1.0., and continues this process until the rule is binary) and generates file accordingly.
+     */
     public void binarizePCFG() {
-        //To binarize a rule, start with the leftmost pair of symbols and replace them with a new nonterminal,
-        //not already in your grammar. For this assignment, we’ll use X followed by some number
-        //to represent these new non-terminals. Then, create a new PCFG rule from X to those two symbols
-        //with probability 1.0. Continue this process until the rule is binary
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("short.binary.pcfg"));
 
-        String prefix = "X";
-        int totalCount = 0;
+            //initializes the prefix and total count
+            String prefix = "X";
+            int totalCount = 1;
 
-        for (String key : rules.keySet()) {
-            for (GrammarRule rule : rules.get(key).keySet()) {
-                String lhs = rule.getLhs();
-                ArrayList<String> rhs = rule.getRhs();
-                if (rhs.size() > 2) {
-                    String prev = rhs.get(0);
-                    for (int i = 1; i < rhs.size() - 1; i++) {
-                        String new_lhs = "X" + i;
+            //goes over all String representations of rules
+            for (String key : rules.keySet()) {
+                for (GrammarRule rule : rules.get(key).keySet()) {
+
+                    //creates a variabloe for the left and right hand sides
+                    String lhs = rule.getLhs();
+                    ArrayList<String> rhs = rule.getRhs();
+
+                    //enters this section if needs to be binarized
+                    if (rhs.size() > 2) {
+
+                        String prev = rhs.get(0);
+
+                        // Binarize rule and write the new rule to the file
+                        for (int i = 1; i < rhs.size() - 1; i++) {
+                            String new_lhs = prefix + totalCount;
+                            ArrayList<String> new_rhs = new ArrayList<>();
+                            new_rhs.add(prev);
+                            new_rhs.add(rhs.get(i));
+                            GrammarRule g = new GrammarRule(new_lhs, new_rhs, 1.0);
+                            writer.write(g.toString());
+                            writer.newLine();
+                            totalCount++;
+                            prev = new_lhs;
+                        }
+
+                        //writes the rule to the file for the last case
                         ArrayList<String> new_rhs = new ArrayList<>();
                         new_rhs.add(prev);
-                        new_rhs.add(rhs.get(i));
-                        GrammarRule g = new GrammarRule(new_lhs, new_rhs, 1.0);
-                        System.out.println(g);
-                        totalCount++;
-                        prev = new_lhs;
-                    }
-                } else {
-                    System.out.println(rule);
-                }
+                        new_rhs.add(rhs.get(rhs.size() - 1));
+                        GrammarRule g2 = new GrammarRule(lhs, new_rhs, rule.getWeight());
+                        writer.write(g2.toString());
+                        writer.newLine();
 
+                    // write rule to file if already binary
+                    } else {
+                        writer.write(rule.toString());
+                        writer.newLine();
+                    }
+
+                }
             }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 
 
-        /*
-        int i = 0;
-        HashMap<String, Double> updatedCounts = new HashMap<>();
-        HashMap<String, HashMap<GrammarRule, Double>> updatedRules = new HashMap<>();
-
-        for (String key : rules.keySet()) {
-            for (GrammarRule rule : rules.get(key).keySet()){
-                if (rule.getRhs().size() > 2) {
-                    for (int j = 0; j < rule.getRhs().size() / 2; j++) {
-                        String xKey;
-                        if (i == 0) {
-                            xKey = "X";
-                        } else {
-                            xKey = "X" + i;
-                        }
-                        ArrayList<String> subList = new ArrayList<>(rule.getRhs().subList(j, j+2));
-                        GrammarRule updatedRule = new GrammarRule(xKey, subList, rule.isLexical());
-                        updatedCounts.put(xKey, 1.0);
-                        updatedRules.put(xKey, new HashMap<>());
-                        updatedRules.get(xKey).put(updatedRule, 1.0);
-                        i++;
-                    }
-
-                    //rule.getRhs().subList(0,2)
-                }
-            }
-        }
-
-        rules.putAll(updatedRules);
-    }
-
-         */
 
     /**
-     * Prints the probabilities of each rule.
+     * Generates the file.
      */
     public void printProbs() {
-        for (String key : rules.keySet()) {
-            for (GrammarRule rule : rules.get(key).keySet()) {
-                System.out.println(rule);
+
+        //creates BufferedWriter
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("short.binary.pcfg"));
+
+            //goes over every rule and writes it to the new file
+            for (String key : rules.keySet()) {
+                for (GrammarRule rule : rules.get(key).keySet()) {
+                    writer.write(rule.toString());
+                    writer.newLine();
+                }
             }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -174,20 +193,18 @@ public class ConstructPCFG {
     public HashMap<String, HashMap<GrammarRule, Double>> getRules() { return rules; }
 
     public static void main(String[] args) {
-        String filename = "/Users/ezraford/git/assign3-starter/example/example.parsed";
-        //String filename = "/Users/talmordoch/Desktop/NLP/assign3-starter/example/example.parsed";
+        //String filename = "/Users/ezraford/git/assign3-starter/example/example.parsed";
+        String filename = "/Users/talmordoch/Desktop/NLP/assign3-starter/data/short.parsed";
         //String filename = "/Users/talmordoch/Desktop/NLP/assign3-starter/data/simple.parsed";
 
         ConstructPCFG pcfg = new ConstructPCFG(filename);
 
-        System.out.println(pcfg.getCounts());
-        System.out.println(pcfg.getRules());
+        //System.out.println(pcfg.getCounts());
+        //System.out.println(pcfg.getRules());
         //pcfg.updateWeight("PP");
 
-        pcfg.printProbs();
-
         pcfg.binarizePCFG();
+        //pcfg.printProbs();
 
-        pcfg.printProbs();
     }
 }
